7d2fa077e80dd2d8237377067228cf8cba77ab3d
yachea <dev.yachea@gmail.com>
2025-06-12 17:48:01 +0900

    [docs] ticket 코드 추가 진행

---
+++ js/ticket.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function () {
+  new Swiper(".sw_ticket", {
+    slidesPerView: 8,
+    spaceBetween: 10,
+    slidesPerGroup: 4,
+    navigation: {
+      nextEl: ".ticket_slide_next",
+      prevEl: ".ticket_slide_prev",
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
