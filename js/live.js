a847a4ebcc0275276565e1515cabc29addd5e81a
yachea <dev.yachea@gmail.com>
2025-06-12 13:16:34 +0900

    [docs] trip 과 live 기본 코드 완료

---
+++ js/live.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function () {
+  // swiper 만들기
+  new Swiper(".sw_live", {
+    slidesPerView: 8,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".live_slide_next",
+      prevEl: ".live_slide_prev",
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        slidesPerGroup: 3,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        slidesPerGroup: 4,
+      },
+    },
+  });
+});
---
+++ js/trip.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", function () {
+  // swiper 만들기
+  new Swiper(".sw_trip", {
+    slidesPerView: 8,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    navigation: {
+      nextEl: ".trip_slide_next",
+      prevEl: ".trip_slide_prev",
+    },
+    breakpoints: {
+      960: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
