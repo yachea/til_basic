573da4b49d968a48fdcf312be4d27504e90f0ffb
yachea <dev.yachea@gmail.com>
2025-07-03 16:58:32 +0900

    [docs] js를 수정작업

--- js/ticket.js
+++ js/ticket.js
@@ -1,214 +1,139 @@
-window.addEventListener("load", function () {
-  // API 데이터
-  const ticketApiData = [
-    {
-      카테고리: "뮤지컬",
-      데이터: [
-        {
-          uid: 1,
-          링크: "#",
-          이미지주소: "images/ticket1.gif",
-          랭킹: "1",
-          제목: "<슬립노모어 서울> (Sleep No More Seoul)",
-          장소: "매키탄 호텔 (The McKithan Hotel)",
-          날짜: "2025.07.24 ~2025.08.09",
-          박스: [
-            { 스타일: "red", 텍스트: "단독판매" },
-            { 스타일: "blue", 텍스트: "좌석우위" },
-          ],
-        },
-        {
-          uid: 2,
-          링크: "#",
-          이미지주소: "images/ticket2.gif",
-          랭킹: "2",
-          제목: "뮤지컬 〈맘마미아!〉",
-          장소: "LG아트센터 서울 LG SIGNATURE 홀",
-          날짜: "2025.07.26 ~ 2025.10.25",
-          박스: [{ 스타일: "red", 텍스트: "단독판매" }],
-        },
-        {
-          uid: 3,
-          링크: "#",
-          이미지주소: "images/ticket3.gif",
-          랭킹: "3",
-          제목: "[NOL 스페셜 스테이지] 뮤지컬 〈팬텀〉 10주년 기념 공연",
-          장소: "세종문화회관 대극장",
-          날짜: "2025.07.12 ~2025.07.17",
-          박스: [{ 스타일: "blue", 텍스트: "좌석우위" }],
-        },
-        {
-          uid: 4,
-          링크: "#",
-          이미지주소: "images/ticket4.gif",
-          랭킹: "4",
-          제목: "뮤지컬 〈위키드〉 내한 공연(WICKED The Musical)",
-          장소: "블루스퀘어 신한카드홀",
-          날짜: "2025.07.12 ~2025.10.26",
-          박스: [
-            { 스타일: "red", 텍스트: "단독판매" },
-            { 스타일: "blue", 텍스트: "좌석우위" },
-          ],
-        },
-        {
-          uid: 5,
-          링크: "#",
-          이미지주소: "images/ticket5.gif",
-          랭킹: "5",
-          제목: "뮤지컬 〈스트라빈스키〉",
-          장소: "대학로 TOM(티오엠) 2관",
-          날짜: "2025.07.28 ~2025.10.12",
-          박스: [{ 스타일: "blue", 텍스트: "좌석우위" }],
-        },
-        {
-          uid: 6,
-          링크: "#",
-          이미지주소: "images/ticket6.gif",
-          랭킹: "6",
-          제목: "뮤지컬 〈마리 퀴리〉",
-          장소: "광림아트센터 BBCH홀",
-          날짜: "2025.07.25 ~2025.10.19",
-          박스: [],
-        },
-        {
-          uid: 7,
-          링크: "#",
-          이미지주소: "images/ticket7.gif",
-          랭킹: "7",
-          제목: "뮤지컬 〈매디슨 카운티의 다리〉",
-          장소: "광림아트센터 BBCH홀",
-          날짜: "2025.05.01 ~2025.07.13",
-          박스: [{ 스타일: "red", 텍스트: "단독판매" }],
-        },
-      ],
-    },
-    {
-      카테고리: "콘서트",
-      데이터: [],
-    },
-    {
-      카테고리: "스포츠",
-      데이터: [],
-    },
-    {
-      카테고리: "전시/행사",
-      데이터: [],
-    },
-    {
-      카테고리: "클래식/무용",
-      데이터: [],
-    },
-    {
-      카테고리: "아동/가족",
-      데이터: [],
-    },
-    {
-      카테고리: "연극",
-      데이터: [],
-    },
-    {
-      카테고리: "레저/캠핑",
-      데이터: [],
-    },
-  ];
+// HTML 완성되면
+window.addEventListener("DOMContentLoaded", function () {
+  // 1. 백엔드 데이터 가져오기 : 1 번만 실행
+  let originData;
 
-  // 카테고리 html 태그 만들기
-  let btHtml = ``;
-  for (let i = 0; i < ticketApiData.length; i++) {
-    const tag = ` <li><button>${ticketApiData[i].카테고리}</button></li>`;
-    btHtml = btHtml + tag;
-  }
-  // 카테고리 버튼 출력장소 및 출력하기
-  const btPos = document.querySelector(".ticket_button_list");
-  btPos.innerHTML = btHtml;
-  // 포커스 스타일 추가하기
-  //  아래 숫자가 포커스 된 카테고리의 순서 이다.
-  let focusIndex = 0;
-  const btlist = document.querySelectorAll(".ticket_button_list li button");
-
-  // focusIndex 번호의 button 태그에 클래스 추가
-  btlist[focusIndex].classList.add("ticket_focus");
-
-  // 클릭 시 포커스 이동하기
-  btlist.forEach(function (item, index) {
-    item.addEventListener("click", function () {
-      focusIndex = index;
-      resetBts();
-    });
-  });
-  // 포커스 리셋 및 최종 포커스 적용하기
-  function resetBts() {
-    btlist.forEach(function (item) {
-      item.classList.remove("ticket_focus");
-    });
-    btlist[focusIndex].classList.add("ticket_focus");
+  async function getData() {
+    try {
+      const res = await fetch("/apis/ticket.json");
+      const result = await res.json();
+      // 전역에서 사용하도록 보관함.
+      originData = result;
+      makeCategoryHtml();
+      makeSlideHtml();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  // html 태그 만들기
-  let html = ``;
-
-  //   실제 데이터 개수 만큼 slide html 만들기
-  html = ``;
-  for (let i = 0; i < ticketApiData[focusIndex].데이터.length; i++) {
-    // 임시 태그
-    let tag = `
-    <div class="swiper-slide">
-  <a href="${ticketApiData[focusIndex].데이터[i].링크}" class="ticket_slide_item">
-    <div class="ticket_image">
-      <img src="${ticketApiData[focusIndex].데이터[i].이미지주소}" alt="${ticketApiData[0].데이터[i].제목}" />
-      <div class="ticket_rank">${ticketApiData[focusIndex].데이터[i].랭킹}</div>
-    </div>
-    <div class="ticket_info">
-      <p class="ticket_item_title">
-        ${ticketApiData[focusIndex].데이터[i].제목}
-      </p>
-      <p class="ticket_item_place">
-        ${ticketApiData[focusIndex].데이터[i].장소}
-      </p>
-      <p class="ticket_item_day">${ticketApiData[focusIndex].데이터[i].날짜}</p>
-      <div class="ticket_item_option">`;
-
-    for (let j = 0; j < ticketApiData[focusIndex].데이터[i].박스.length; j++) {
-      tag =
-        tag +
-        `<span class="ticket_${ticketApiData[focusIndex].데이터[i].박스[j].스타일}">${ticketApiData[focusIndex].데이터[i].박스[j].텍스트}</span>`;
+  // 2. html 생성
+  // 2.1. 카테고리 버튼 생성
+  function makeCategoryHtml() {
+    let cateHtml = "";
+    for (let i = 0; i < originData.length; i++) {
+      const obj = originData[i];
+      const tag = `<li><button>${obj.카테고리}</button></li>`;
+      cateHtml = cateHtml + tag;
     }
 
-    tag =
-      tag +
-      `</div>
-    </div>
-  </a>
-</div>
-    `;
+    const where = document.querySelector(".ticket_button_list");
 
-    html = html + tag;
+    where.innerHTML = cateHtml;
+    makeBtnInit();
+  }
+  // 2.2. 슬라이드 html 태그 생성
+  let showIndex = 0; // 보여주어야 할 배열의 인덱스 보관
+  function makeSlideHtml() {
+    let html = "";
+    // 현재 배열의 몇번째 데이터를 보여주여야 하는가?
+    const showData = originData[showIndex].데이터;
+
+    for (let i = 0; i < showData.length; i++) {
+      const obj = showData[i];
+
+      // 임시 태그
+      let tag = `
+    <div class="swiper-slide">
+      <a href="${obj.링크}" class="ticket_slide_item">
+        <div class="ticket_image">
+          <img
+            src="${obj.이미지}"
+            alt="${obj.타이틀}"
+          />
+          <div class="ticket_rank">${obj.랭크}</div>
+        </div>
+
+        <div class="ticket_info">
+          <p class="ticket_item_title">
+            ${obj.타이틀}
+          </p>
+          <p class="ticket_place">
+            ${obj.장소}
+          </p>
+          <p class="ticket_day">${obj.날짜}</p>
+          <div class="ticket_item_option">`;
+
+      for (let j = 0; j < obj.박스.length; j++) {
+        tag =
+          tag +
+          `<span class="ticket_${obj.박스[j].스타일}">${obj.박스[j].텍스트}</span>`;
+      }
+
+      tag =
+        tag +
+        `</div>
+        </div>
+      </a>
+    </div> 
+    `;
+      html = html + tag;
+    }
+    const where = document.querySelector(".sw_ticket .swiper-wrapper");
+    where.innerHTML = html;
+
+    makeSlide();
+  }
+  // 3.  Swiper 를 만들기
+  let swiperTicket;
+  function makeSlide() {
+    if (swiperTicket) {
+      swiperTicket.destroy();
+    }
+    // swiper 만들기 실행
+    new Swiper(".sw_ticket", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+      navigation: {
+        nextEl: ".ticket_slide_next",
+        prevEl: ".ticket_slide_prev",
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1280: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+    });
   }
 
-  // 태그 출력 장소 지정하기
-  const ticketPos = document.querySelector(".sw_ticket .swiper-wrapper");
-  ticketPos.innerHTML = html;
+  // 4. 카테고리 버튼 선택시 다시 슬라이드 html 태그 생성
+  //     2.2 과정,   3 과정을 다시 진행
+  // 5. 버튼 포커스 기능
+  // 기능은 1번만 등록하기
+  function makeBtnInit() {
+    const btList = document.querySelectorAll(".ticket_button_list li button");
+    btList[showIndex].classList.add("ticket_focus");
+    btList.forEach(function (item, index) {
+      item.addEventListener("click", function () {
+        resetFocus();
+        showIndex = index;
+        item.classList.add("ticket_focus");
+        makeSlideHtml();
+      });
+    });
+  }
 
-  //  swiper 슬라이드
-  new Swiper(".sw_ticket", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    slidesPerGroup: 3,
-    navigation: {
-      nextEl: ".ticket_slide_next",
-      prevEl: ".ticket_slide_prev",
-    },
-    breakpoints: {
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        slidesPerGroup: 3,
-      },
-      1280: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        slidesPerGroup: 4,
-      },
-    },
-  });
+  // 6. 포커스 해제 기능
+  function resetFocus() {
+    const btList = document.querySelectorAll(".ticket_button_list li button");
+    btList.forEach(function (item) {
+      item.classList.remove("ticket_focus");
+    });
+  }
+  getData();
 });
